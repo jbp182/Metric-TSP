56a7e243fa065009d097b490b5659a62b9e1722a
Hao <h.chen@campus.fct.unl.pt>
2020-06-02 01:06:53 +0100

    Linear Generator

--- TP1-MetricTSP/src/generate/TestGenerator.java
+++ TP1-MetricTSP/src/generate/TestGenerator.java
@@ -6,15 +6,17 @@ public class TestGenerator {
 	public static void main(String[] args) {
 		
 		int NUM_TESTS = 2;
-		int NUM_NODES = 100;
+		int NUM_NODES = 300;
 		int MAX_SCALE = 100;
 		String FILENAME = "tests/test";
 		
 		
 		for (int i = 0; i < NUM_TESTS; i++) {
 			String name = String.format("%s_%d_%d_%d.txt", FILENAME, MAX_SCALE, NUM_NODES, i);
-			GraphGenerator.generateGraph(name, NUM_NODES, MAX_SCALE);
+			//GraphGenerator.generateGraph(name, NUM_NODES, MAX_SCALE);
+			GraphGenerator_Linear.generateGraph(name, NUM_NODES, MAX_SCALE);
 		}
+		System.out.println("done");
 		
 	}
 

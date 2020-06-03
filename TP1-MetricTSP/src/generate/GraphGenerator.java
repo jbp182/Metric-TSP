9ee225d50b1d3b3f6ab5f487aa7ad88fbccc6661
Hao <h.chen@campus.fct.unl.pt>
2020-06-03 01:21:01 +0100

    new Generator
    juntei as funcoes para uma classe de generator.

--- TP1-MetricTSP/src/generate/GraphGenerator.java
+++ TP1-MetricTSP/src/generate/GraphGenerator.java
@@ -43,6 +43,112 @@ public class GraphGenerator {
 		
 	}
 	
+	public static void generateGraphLinear(String filename, int numNodes, int maxScale) {
+		
+		Point[] points = new Point[numNodes];
+
+		int y0 = (int)(Math.random() * maxScale);
+		for (int i = 0; i < numNodes; i++) {
+			int x = (int)(Math.random() * maxScale);
+			points[i] = new Point(x,y0);
+		}
+		
+		int[][] dist = new int[numNodes][numNodes];
+		
+		for(int i = 0; i < numNodes - 1; i++) {
+			for (int j = i+1; j < numNodes; j++) {
+				int d = (int)points[i].getDistanceTo(points[j]);
+				dist[i][j] = d;
+				dist[j][i] = d;
+			}
+		}
+		
+		try {
+			FileWriter writer = new FileWriter(new File(filename));
+			
+			for (int row = 0; row < numNodes; row++) {
+				for (int col = 0; col < numNodes; col++) {
+					writer.write(dist[row][col] + " ");
+				}
+				writer.write("\n");
+			}
+			
+			writer.close();
+		} catch (IOException e) {
+			System.err.println("Error while creating file " + filename + ".");
+		}
+		
+	}
 	
+	public static void generateGraphSquare(String filename, int numNodes, int maxScale) {
+		
+		
+		Point[] points = new Point[numNodes];
+
+		int numEdges = 4;
+		int numPointInEdge = numNodes / numEdges;
+		
+		int k = 0;
+		// pin x0
+		//vertical edge 1
+		int x0 = (int)(Math.random() * maxScale);
+		for (; k < numPointInEdge; k++) {
+			int y = (int)(Math.random() * maxScale);
+			points[k] = new Point(x0,y);
+		}
+		//pin x1
+		// vertical edge 2
+		int x1 = (int)(Math.random() * maxScale);
+		for (; k < numPointInEdge*2; k++) {
+			int y = (int)(Math.random() * maxScale);
+			points[k] = new Point(x1,y);
+		}
+		
+		//pin y0
+		//honrizontal edge 1
+		int y0 = (int)(Math.random() * maxScale);
+		for (; k < numPointInEdge*3; k++) {
+			int x = (int)(Math.random() * maxScale);
+			points[k] = new Point(x,y0);
+		}
+		
+		
+
+		int y1 = (int)(Math.random() * maxScale);
+		for(; k < numNodes;k++) {
+			int x = (int)(Math.random() * maxScale);
+			points[k] = new Point(x,y1);
+			
+		}
+		
+		
+		int[][] dist = new int[numNodes][numNodes];
+		
+		for(int i = 0; i < numNodes - 1; i++) {
+			for (int j = i+1; j < numNodes; j++) {
+				int d = (int)points[i].getDistanceTo(points[j]);
+				dist[i][j] = d;
+				dist[j][i] = d;
+			}
+		}
+		
+		try {
+			FileWriter writer = new FileWriter(new File(filename));
+			
+			for (int row = 0; row < numNodes; row++) {
+				for (int col = 0; col < numNodes; col++) {
+					writer.write(dist[row][col] + " ");
+				}
+				writer.write("\n");
+			}
+			
+			writer.close();
+		} catch (IOException e) {
+			System.err.println("Error while creating file " + filename + ".");
+		}
+		
+	}
+	
+
 	
 }
--- TP1-MetricTSP/src/generate/TestGenerator.java
+++ TP1-MetricTSP/src/generate/TestGenerator.java
@@ -3,21 +3,47 @@ package generate;
 public class TestGenerator {
 
 
+	public static final int RANDOM = 0;
+	public static final int LINEAR = 1;
+	public static final int SQUARE = 2;
+	
+	
 	public static void main(String[] args) {
 		
 		int NUM_TESTS = 2;
-		int NUM_NODES = 300;
+		int NUM_NODES = 50;
 		int MAX_SCALE = 100;
 		String FILENAME = "tests/test";
 		
 		
-		for (int i = 0; i < NUM_TESTS; i++) {
+		for (int i = 2; i < 4; i++) {
 			String name = String.format("%s_%d_%d_%d.txt", FILENAME, MAX_SCALE, NUM_NODES, i);
-			//GraphGenerator.generateGraph(name, NUM_NODES, MAX_SCALE);
-			GraphGenerator_Linear.generateGraph(name, NUM_NODES, MAX_SCALE);
+			generateGraph(SQUARE, name, NUM_NODES, MAX_SCALE);
 		}
 		System.out.println("done");
 		
 	}
+	
+	
+	public static void generateGraph(int mode,String filename, int numNodes, int maxScale) {
+		switch(mode) {
+		case RANDOM:
+			GraphGenerator.generateGraph(filename, numNodes, maxScale);
+			
+			break;
+		case LINEAR:
+			GraphGenerator.generateGraphLinear(filename, numNodes, maxScale);
+			
+			break;
+		case SQUARE:
+			GraphGenerator.generateGraphSquare(filename, numNodes, maxScale);
+			
+			break;
+		default:
+			System.out.println("This mode does not exist");
+		}
+	}
+	
+	
 
 }
